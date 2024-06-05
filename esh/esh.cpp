2d06375cd5d3cb0d41b33454d4c7c83ec0728676
Ambrose Karella <ajkarella@gmail.com>
2024-06-05 08:05:58 -0800

    line history and exec

--- esh/esh.cpp
+++ esh/esh.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+using namespace std;
  
 //TODO: on start, open cache file, load it into string array, close cache
 // https://www.tutorialspoint.com/how-to-read-a-text-file-with-cplusplus
 
 //TODO: on exit, open cache file, save current string array, close cache
 // https://stackoverflow.com/questions/9402254/how-do-you-run-a-function-on-exit-in-c
-
+/*
 int main()
 {
   // Initialize 2D array
@@ -22,4 +29,21 @@ int main()
       std::cout << colour[i] << "\n";
  
   return 0;
+}
+*/
+
+int main()
+{
+    //char cmd[1000];
+    char *cmd;
+    while(1){
+        //cout << "esh> ";
+        cmd = readline("esh> ");
+        add_history(cmd);
+        //cin >> cmd;
+        system(cmd);
+        free(cmd);
+    }
+ 
+    return 0;
 }
